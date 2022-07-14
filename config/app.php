6da28296fe28bf4dba6b79d42c8677b42345f2d4
Edith008 <edith.sossa.c98@gmail.com>
2022-07-13 22:46:39 -0400

    solucion de form

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -212,6 +213,9 @@ return [
         // 'ExampleClass' => App\Example\ExampleClass::class,
         
         'PDF' => Barryvdh\DomPDF\Facade::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+
    
         ])->toArray(),
 
