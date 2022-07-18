82f6a459fef60be0a6d8ec53a69fa3ddc6d32c6f
Edith008 <edith.sossa.c98@gmail.com>
2022-07-18 11:58:11 -0400

    titulo de nuevo socio

--- resources/views/socio/create.blade.php
+++ resources/views/socio/create.blade.php
@@ -13,7 +13,7 @@
 
                 <div class="card card-default">
                     <div class="card-header">
-                        <span class="card-title">Create Socio</span>
+                        <span class="card-title">Registro de nuevo socio </span>
                     </div>
                     <div class="card-body">
                         <form method="POST" action="{{ route('socios.store') }}"  role="form" enctype="multipart/form-data">
